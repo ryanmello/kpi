02d90b95954a7749e4f43d43c797dea98bcab184
Ryan Mello <ryanmello897@gmail.com>
2024-10-04 19:14:43 -0700

    task api routes

---
+++ app/api/task/delete/route.ts
@@ -0,0 +1,27 @@
+import { auth } from "@/auth";
+import { getUserByEmail } from "@/data/user";
+import { NextResponse } from "next/server";
+import db from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const session = await auth();
+    const user = await getUserByEmail(session?.user?.email);
+
+    if (user == null || user.role != "admin")
+      return new NextResponse("Unauthorized", { status: 401 });
+
+    const { id } = await req.json();
+
+    const task = await db.task.delete({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("/api/task/delete", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ app/api/task/update/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@/auth";
+import { getUserByEmail } from "@/data/user";
+import { NextResponse } from "next/server";
+import db from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const session = await auth();
+    const user = await getUserByEmail(session?.user?.email);
+
+    if (user == null || user.role != "admin")
+      return new NextResponse("Unauthorized", { status: 401 });
+
+    const requestData = await req.json();
+    const {
+      id,
+      description,
+      startDate,
+      endDate,
+      timeSpent,
+      progressAchieved,
+      status,
+      comments,
+    } = requestData;
+
+    const task = await db.task.update({
+      where: {
+        id,
+      },
+      data: {
+        description,
+        startDate,
+        endDate,
+        timeSpent,
+        progressAchieved,
+        status,
+        comments,
+      },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("/api/task/update", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
