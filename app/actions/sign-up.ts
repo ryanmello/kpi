7e3269168890a22d25567c6af78efbbfd7c467b1
Ryan Mello <ryanmello897@gmail.com>
2024-12-06 09:43:10 -0800

    call sign in after creating an account

--- app/actions/sign-up.ts
+++ app/actions/sign-up.ts
@@ -5,6 +5,7 @@ import { SignUpSchema } from "@/schemas";
 import * as bcrypt from "bcryptjs";
 import db from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { signIn } from "./sign-in";
 
 export const signUp = async (values: z.infer<typeof SignUpSchema>) => {
   const validatedFields = SignUpSchema.safeParse(values);
@@ -29,6 +30,12 @@ export const signUp = async (values: z.infer<typeof SignUpSchema>) => {
       password: hashedPassword,
     },
   });
+
+  const signInResult = await signIn({ email, password });
+
+  if (signInResult && signInResult.error) {
+    return { error: signInResult.error };
+  }
 
   return { success: "User created " };
 };
