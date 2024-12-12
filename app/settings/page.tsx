b2612db2b6503d419c45958bf2d1cdbfc74c7b4e
Ryan Mello <ryanmello897@gmail.com>
2024-12-11 20:01:28 -0800

    sign out

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,10 +1,19 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const Settings = async () => {
   const session = await auth();
   return (
     <div>
       <p>{JSON.stringify(session)}</p>
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
     </div>
   );
 };
