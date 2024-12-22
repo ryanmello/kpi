90766a91d37236bb32ec36b0b5b841dcb6648534
Ryan Mello <ryanmello897@gmail.com>
2024-12-22 09:19:40 -0800

    update middleware routes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react";
-import HomePage from "./components/Home";
-
 export default function page() {
   return (
     <div>
       <h1>KPI</h1>
-      <Button>Click me</Button>
-      <HomePage />
     </div>
   );
 }
--- routes.ts
+++ routes.ts
@@ -1,6 +1,6 @@
-export const publicRoutes = ["/"];
+export const publicRoutes = [];
 
 export const authRoutes = ["/sign-in", "/sign-up"];
 
 export const apiAuthPrefix = "/api/auth";
-export const DEFAULT_SIGN_IN_REDIRECT = "/settings";
+export const DEFAULT_SIGN_IN_REDIRECT = "/";
